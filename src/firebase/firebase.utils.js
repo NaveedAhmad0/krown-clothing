e72cd8cad55ac6c54bec5beee3fa09505a9305c1
NaveedAhmad0 <naveedahmad9501@gmail.com>
2022-07-26 19:19:19 +0530

    implemented firebase utils to store user in db and app from there email and pswd

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,11 @@
 import { initializeApp } from "firebase/app";
-
-// import "firebase/firestore";
+import {
+	setDoc,
+	getDoc,
+	doc,
+	onSnapshot,
+	getFirestore,
+} from "firebase/firestore";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 import "firebase/auth";
@@ -22,11 +27,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-// export const firestore = firebase.firestore();
-
-// provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => {
 	const provider = new GoogleAuthProvider();
@@ -37,4 +38,54 @@ export const signInWithGoogle = () => {
 		.catch((error) => {
 			console.log(error);
 		});
+};
+const db = getFirestore();
+export default onSnapshot;
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = doc(db, "users", userAuth.uid);
+	const snapShot = await getDoc(userRef);
+	// const snapShot = await getDoc(userRef).then((doc) => {
+	// console.log(userAuth.uid);
+	// });
+	if (!snapShot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userRef, {
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("Error creating user", error.message);
+		}
+	}
+	// if (snapShot.exists) {
+	// 	// alert("user Already exists");
+	// 	console.log("user already exists", snapShot.exists());
+	// }
+	return userRef;
+	// const docRef = doc(db, "users", "uaz4VWyJb3XPPhQ0Y3YZiI87Bph1");
+
+	// const docSnap = await getDocs(colRef)
+	// 	.then((snapshot) => {
+	// 		let users = [];
+	// 		snapshot.docs.forEach((doc) => {
+	// 			users.push({ ...doc.data(), id: doc.id });
+	// 		});
+	// 		console.log(users);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err.message);
+	// 	});
+
+	// if (docSnap) {
+	// 	console.log("Document data:", docSnap.data());
+	// } else {
+	// 	// doc.data() will be undefined in this case
+	// 	console.log("No such document!");
+	// }
 };
